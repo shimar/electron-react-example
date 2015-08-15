c92c034ba1c33056fbdfa2239cff7118c9bb598e
shimar <shima01dev@gmail.com>
2015-08-15 15:15:55 +0900

    enable gulp.

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+
+// gulp.task('jade', function() {
+//   gulp.src('renderer/views/jade/**/*.jade')
+//   .pipe($.jade({
+//     pretty: true
+//   }))
+//   .pipe(gulp.dest('renderer/views'));
+// });
+
+// gulp.task('styles', function() {
+//   return gulp.src('renderer/styles/less/main.less')
+//          .pipe($.plumber())
+//          .pipe($.less({ paths: ['.'] }))
+//          .pipe(gulp.dest('renderer/styles'));
+// });
+
+gulp.task('watch', function() {
+  // gulp.watch('renderer/views/**/*.jade',  ['jade']);
+  // gulp.watch('renderer/styles/**/*.less', ['styles']);
+});
+
+// gulp.task('run', [ 'jade', 'styles', 'watch' ], function() {
+//   return $.run('electron .').exec();
+// });
+
+gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
+
+gulp.task('default', ['clean'], function () {
+  console.log('this default task.');
+});
